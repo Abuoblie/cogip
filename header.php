8b1842482b166d2f3ca192250a47b4392618a777
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-04-07 11:23:30 +0200

    update

--- header.php
+++ header.php
@@ -9,3 +9,32 @@
         <title>codip</title>
 </head>
 <body>
+	<header>
+        <nav class="navbar navbar-expand-lg navbar-light navagation">
+            <div class="container-fluid" >
+              <button class="navbar-toggler buttonBackground "  type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon" ></span>
+              </button>
+              <a class="navbar-brand " href="#"> <img class="img-fluid " height="" width="" src="assets/logo.png" alt="foto"></a>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link  " aria-current="page" href="">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="">Invoices</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="companiesPage.php" tabindex="-1" aria-disabled="true">Companies</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="contactPage.php" tabindex="-1" aria-disabled="true">Contacts</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="" tabindex="-1" aria-disabled="true">Admin</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+   </header>

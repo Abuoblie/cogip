7be4b94ac26c9413000221a29cdc61bd1ba57dcc
Maureenm41 <maureenmrchl@gmail.com>
2021-04-07 16:39:51 +0200

    update

--- clients.php
+++ clients.php
@@ -1,7 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>clients</title>
+    <link href="Assets/login.css" rel="stylesheet"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
 <?php 
   require_once "Assets/header.php";
 ?>
 
+<h1>Welcome to the COGIP</h1>
+
+
+
 <?php 
   require_once "Assets/footer.php ";
 ?>
+</body>
+</html>

85e5d66e3e1c2c9bddf744adf11b51308655acae
Maureenm41 <maureenmrchl@gmail.com>
2021-04-09 15:21:33 +0200

    update welcome

--- Assets/ProcessHandler.php
+++ Assets/ProcessHandler.php
@@ -25,15 +25,7 @@ class Handle extends dbh{
         }
        
         //This page will display a list of all the invoices from the most recent to the oldest. Each invoice number will be a link to a new page detailing the invoice, the content will be generated with the ID of the chosen invoice.
-        public function getInvoice($condition , $check)
-        {
-                $sql = "SELECT * FROM Invoice where $condition =?  ORDER by id_Invoice DESC";
-                $stmt = $this->connect()->prepare($sql);
-                $stmt->execute([$check]);
-                $result = $stmt -> fetchAll();  
-                return $result; 
-                exit();     
-        }
+
        
        
         //Contacts page
@@ -55,20 +47,10 @@ class Handle extends dbh{
 
         
 
-        public function getCompanies($condition , $check)
-        {
-                $sql = "SELECT * FROM Company where $condition =?  ORDER by name";
-                $stmt = $this->connect()->prepare($sql);
-                $stmt->execute([$check]);
-                $result = $stmt -> fetchAll();  
-                return $result; 
-                exit(); 
-                
-        }
         // 3Providers page
          //This page will display a list of all providers in alphabetical order. The name of the provider will be a link to a new page detailing the provider, the content will be generated with the ID of the chosen provider. (same detailing page as for companies)
 
-         public function getProviders($condition , $check)
+         public function getCompanies($condition , $check)
          {
                  $sql = "SELECT * FROM Company as c join Type t on c.id_Type = t.id_Type where $condition =?  ORDER by name";
                  $stmt = $this->connect()->prepare($sql);
@@ -113,7 +95,21 @@ class Handle extends dbh{
                 exit();
         }
 
+        public function getInvoice($condition, $check)
+        {
+                $sql = "SELECT * FROM Company as c join People p on c.id_Company = p.id_Company join invoice i on i.id_People = p.id_People where $condition =? ORDER by invoice_date";
+                $stmt = $this->connect()->prepare($sql);
+                $stmt->execute([$check]);
+                $result = $stmt->fetchAll();
+                return $result;
+                exit();
+        }
+
+
 }
+
+
+
 
 
 
--- welcome.php
+++ welcome.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>welcome</title>
-    <link href="Assets/login.css" rel="stylesheet"> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
-<body>
+
 <?php 
   require_once "Assets/header.php";
 ?>
@@ -20,19 +10,109 @@
 
 <p>Last invoice :</p>
 
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Compagny Name</th>
+      <th scope="col">Invoice Number</th>
+      <th scope="col">Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    
+    $clients = new Handle () ;
+    $resultat = $clients->getInvoice(1,1) ;
+    foreach ($resultat as  $row ) {
+      echo "
+          <tr>
+            <td>{$row['name']}</td>
+            <td>{$row['number']}</td>
+            <td>{$row['invoice_date']}</td>
+          </tr>
+      ";
+
+    }
+    ?>
+  </tbody>
+</table>
+  </tbody>
+</table>
+
 <?php ?> <!--insert data base mysql here-->
 
 
 <p>Last contacts :</p>
 
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Firstname</th>
+      <th scope="col">Lastname</th>
+      <th scope="col">Email</th>
+      <th scope="col">Company</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    
+    $clients = new Handle () ;
+    $resultat = $clients->getContact(1,1) ;
+    foreach ($resultat as  $row ) {
+      echo "
+          <tr>
+            <td>{$row['first_name']}</td>
+            <td>{$row['last_name']}</td>
+            <td>{$row['email']}</td>
+            <td>{$row['name']}</td>
+          </tr>
+      ";
+
+    }
+    ?>
+  </tbody>
+</table>
+  </tbody>
+</table>
+
 <?php ?><!--insert data base mysql here-->
 
 <p>Last compagnies :</p>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Compagny Name</th>
+      <th scope="col">Country</th>
+      <th scope="col">Email</th>
+      <th scope="col">Vat</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+
+    
+    $clients = new Handle () ;
+    $resultat = $clients->getCompanies(1,1) ;
+    foreach ($resultat as  $row ) {
+      echo "
+          <tr>
+            <td>{$row['name']}</td>
+            <td>{$row['country']}</td>
+            <td>{$row['lebel']}</td>
+            <td>{$row['vat']}</td>
+          </tr>
+      ";
+
+    }
+    ?>
+  </tbody>
+</table>
+  </tbody>
+</table>
 
 <?php ?><!--insert data base mysql here-->
 
 <?php 
   require_once "Assets/footer.php ";
 ?>
-</body>
-</html>

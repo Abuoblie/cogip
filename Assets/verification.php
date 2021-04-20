0b89d66a50edddaaf34686ce4e21352b9fcc5c16
Abuoblie <Abu.oblie@gmail.com>
2021-04-20 16:06:34 +0200

    bugfixed verification

--- Assets/verification.php
+++ Assets/verification.php
@@ -13,6 +13,7 @@ require_once "ProcessHandler.php";
              }
              else {
                      echo " invalid login";
+                     header("location: login.php");
              }
      }
 
@@ -29,18 +30,18 @@ require_once "ProcessHandler.php";
                                 $email = null;
                                 $id_Company = null;
                                 $Telephone =  null;
-                                $pswd =sha1($_POST['pswd']);
+                               
 
                                 if (preg_match("/^[\w\s.]+$/", $_POST['firstName'])) {
                                         $firstName = htmlentities(trim(filter_var($_POST['firstName'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../login.php? fname= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                                 if (preg_match("/^[\w\s.]+$/", $_POST['lastName'])) {
                                         $lastName = htmlentities(trim(filter_var($_POST['lastName'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../login.php?lname= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                                 if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
@@ -48,26 +49,26 @@ require_once "ProcessHandler.php";
                                         
 
                                 } else {
-                                        header("Location: ../login.php?email= invalid");
+                                        echo "invalid data";;
                                         exit();
                                 }
                                 if (preg_match('/^\d+$/', $_POST['id_company'])) {
                                         $id_company = htmlentities(trim(filter_var($_POST['id_company'], FILTER_SANITIZE_NUMBER_INT)));
                                 } else {
-                                        header("Location: ../login.php? id_company= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
 
                                 if (preg_match("/^[\w\s.]+$/", $_POST['Telephone'])) {
                                         $Telephone = htmlentities(trim(filter_var($_POST['Telephone'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../login.php? fname= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
 
 
-                                if (empty($_GET['fname']) && empty($_GET['lname']) && empty($_GET['id_company']) && empty($_GET['email'])) {
-                                        $this->insertPeople($firstName, $lastName, $email, $pswd, $id_Company,  $Telephone);
+                                if ($firstName && $lastName && $email && $pswd && $id_Company &&  $Telephone) {
+                                        $this->insertPeople($firstName, $lastName, $email, null, $id_Company,  $Telephone);
                                         echo "contact successfully created";
                                 }
                                 else {
@@ -95,29 +96,29 @@ require_once "ProcessHandler.php";
                                 if (preg_match("/^[\w\s\d\.]+$/", $_POST['name'])) {
                                         $name = htmlentities(trim(filter_var($_POST['firstName'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../login.php? name= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                                 if (preg_match("/^[\w\s\d\.]+$/", $_POST['country'])) {
                                         $country = htmlentities(trim(filter_var($_POST['lastName'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../login.php?country= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                                 if (preg_match("/^[\w\s\d\.]+$/", $_POST['vat'])) {
                                         $vat = htmlentities(trim(filter_var($_POST['vat'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../login.php? vat= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                              
                                 if (preg_match('/^\d+$/', $_POST['id_Type'])) {
                                         $id_Type = htmlentities(trim(filter_var($_POST['id_Type'], FILTER_SANITIZE_NUMBER_INT)));
                                 } else {
-                                        header("Location: ../login.php? id_Type= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
-                                if (empty($_GET['name']) && empty($_GET['country']) && empty($_GET['id_Type']) && empty($_GET['vat'])) {
+                                if ($name && $country && $vat && $id_Type) {
                                         $this->insertCompany($name, $country, $vat, $id_Type);
                                         echo "contact successfully created";
                                 }
@@ -144,14 +145,14 @@ require_once "ProcessHandler.php";
                                 if (preg_match("/^[\w\s\d\.]+$/", $_POST['name'])) {
                                         $number = htmlentities(trim(filter_var($_POST['$number'], FILTER_SANITIZE_STRING)));
                                 } else {
-                                        header("Location: ../create.php? number= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
 
                                 if (preg_match("/^[\w\s\d\-]+$/", $_POST['invoice_date'])) {
                                         $invoice_date = htmlentities(trim($_POST['invoice_date']));
                                 } else {
-                                        header("Location: ../login.php?invoice_date= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                                
@@ -159,11 +160,11 @@ require_once "ProcessHandler.php";
                                 if (preg_match('/^\d+$/', $_POST['id_People'])) {
                                         $id_Type = htmlentities(trim(filter_var($_POST['id_Type'], FILTER_SANITIZE_NUMBER_INT)));
                                 } else {
-                                        header("Location: ../login.php? id_People= invalid");
+                                        echo "invalid data";
                                         exit();
                                 }
                                 
-                                if (empty($_GET['number']) && empty($_GET['invoice_date']) && empty($_GET['id_People'])) {
+                                if ($number && $invoice_date && $id_People) {
                                         $this->insertInvoice($number, $invoice_date, $id_People);
                                         echo "contact successfully created";
                                 }

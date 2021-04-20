453f375af450f77f1ba1494dea899c9c9fc0d6a1
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-04-20 14:49:48 +0200

    update

--- contactPage.php
+++ contactPage.php
@@ -1,5 +1,6 @@
 <?php
 require_once "header.php";
+require_once "verification.php";
 ?>
 <div class="container-md">
     <?php
@@ -7,9 +8,48 @@ require_once "header.php";
         $first_name = '';
         $last_name= '';
         $email= '';
-        $pswd='';
+        $phone='';
+        $company='';
     ?>
-    
+    <div class="row justify-content-center">
+		<form action="verification.php" method="POST">
+            
+			<div
+				style="width: 50%; height: 30%; position: relative; margin-left: auto; margin-right: auto;">
+				<div class="form-group mb-2">
+					<label class="control-label" for="first_name">First Name:</label> <input
+						type="text" id="first_name" required maxlength="48"
+						name="first_name" class="form-control" value="<?php echo $first_name;?>"
+						placeholder="Enter your First Name">
+				</div>
+				<div class="form-group mb-2">
+					<label class="control-label" for="last_name">Last Name:</label> <input
+						type="text" id="last_name" required maxlength="48" name="last_name"
+						class="form-control" value="<?php echo $last_name;?>" placeholder="Enter your Surname">
+				</div>
+				<div class="form-group mb-2">
+					<label class="control-label" for="telephone">Telephone</label> <input
+						type="text" id="telephone" required maxlength="48" name="telephone"
+						class="form-control" value="<?php echo $phone;?>" placeholder="Enter your Phone Number">
+				</div>
+				<div class="form-group mb-2">
+					<label class="control-label" for="email">Email:</label> <input
+						type="text" id="email" required maxlength="48" name="email"
+						class="form-control" value="<?php echo $email;?>" placeholder="Enter your Email">
+				</div>
+				<div class="form-group mb-2">
+					<label class="control-label" for="id_Company">Company:</label> 
+					<input type="id_Company" id="id_Company" required maxlength="48" name="id_Company"
+						class="form-control" value="<?php echo $company;?>" placeholder="Enter your Company">
+				</div>
+
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary" name="submit">Submit</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
 
 
 <?php

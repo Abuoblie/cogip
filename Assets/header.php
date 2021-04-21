1bb31e7fe1e9146ebcaeb440528ca7c3d2afb18f
Abuoblie <Abu.oblie@gmail.com>
2021-04-21 14:21:44 +0200

    verified insertpeople

--- Assets/header.php
+++ Assets/header.php
@@ -1,9 +1,9 @@
 <?php
   session_start();
-  require_once "/COGIP/Assets/ProcessHandler.php";
+  require_once "Verification.php";
   
   if (!isset($_SESSION['email'])) {
-    header("location:/COGIP/Assets/Login.php");
+    //header("location:/COGIP/Assets/Login.php");
   }
   else{
       echo "welcome  {$_SESSION['first_name']} {$_SESSION['last_name']} ";
@@ -17,6 +17,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <title>codip</title>
+        <title>cogip</title>
 </head>
 <body>

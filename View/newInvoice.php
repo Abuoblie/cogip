fdec0d38ad8b4cd841b76831db1578ce6261c342
Abu Oblie <76572108+Abuoblie@users.noreply.github.com>
2021-04-26 16:27:16 +0200

    Update newInvoice.php

--- View/newInvoice.php
+++ View/newInvoice.php
@@ -1,5 +1,5 @@
 <?php
-require_once "View/header.php";
+require_once "header.php";
 
 
 
@@ -74,5 +74,5 @@ require_once "View/header.php";
 	</div>
 </div>
 <?php
-require_once "View/footer.php";
+require_once "footer.php";
 ?>

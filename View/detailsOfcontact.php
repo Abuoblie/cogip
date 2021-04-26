723ab48bffe59a36a155374bbffde6945e0b72b6
Abu Oblie <76572108+Abuoblie@users.noreply.github.com>
2021-04-26 16:20:50 +0200

    Update detailsOfcontact.php

--- View/detailsOfcontact.php
+++ View/detailsOfcontact.php
@@ -1,5 +1,5 @@
 <?php
-require_once "View/header.php";
+require_once "header.php";
 
 
     
@@ -40,5 +40,5 @@ require_once "View/header.php";
     </table>
 </div>
 <?php
-require_once "View/footer.php";
+require_once "footer.php";
 ?>
